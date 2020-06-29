65cacdbe6881080b1abbb01f8266afeac0d58433
VaporyPower72 <signupalex19@gmail.com>
2020-06-29 19:43:26 +0200

    Added multiple pokemons

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+
+import Title from "./components/Title";
+import Pokemon from "./components/Pokemon/Pokemon";
+
+const all_pokemon = [
+  {
+    name: "Charizard",
+    weight: 90,
+    awesome: true,
+    terrifying: false,
+    abilities: ["Blaze", "Solar power", "Tough claws", "Drought"],
+  },
+  {
+    name: "Bulbasaur",
+    weight: 6.9,
+    awesome: true,
+    terrifying: false,
+    abilities: ["Overgrow", "Chlorophyll"],
+  },
+  {
+    name: "Mewtwo",
+    weight: 122,
+    awesome: true,
+    terrifying: true,
+    abilities: ["Pressure", "Unnerve", "Steadfast", "Insomnia"],
+  },
+  {
+    name: "Mega beedrill",
+    weight: 65,
+    awesome: false,
+    terrifying: true,
+    abilities: ["Intimidate", "Unnerve"],
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>JavaScript is the top dog </p>
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Welcome to Codaisseur!
-      </a>
-    </div>
+    <main className="container">
+      <Title content="Gotta catch 'em all!" />
+      <div className="row">
+        {all_pokemon.map((pokemon) => (
+          <div className="col-md-6 col-lg-4">
+            <Pokemon
+              name={pokemon.name}
+              weight={pokemon.weight}
+              awesome={pokemon.awesome}
+              terrifying={pokemon.terrifying}
+              abilities={pokemon.abilities}
+            />
+          </div>
+        ))}
+      </div>
+    </main>
   );
 }
 
